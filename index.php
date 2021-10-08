9c89442d18e85f62f3a206a5834bab5be0568fc3
Ali Qorbani <ghorbani2006@gmail.com>
2021-10-08 20:06:43 +0330

    add insert-into-sample.http some changes on index.php

--- index.php
+++ index.php
@@ -5,10 +5,10 @@ require_once 'responseClass.php';
 $mongo          = new mongoClass();
 $response_class = new responseClass();
 $response       = $response_class->processRequest();
-$count          = isset($response[ 'count' ]) ? $response[ 'count' ] : count($response);
-$result         = isset($response[ 'result' ]) ? $response[ 'result' ] : $response;
-$success        = isset($response[ 'success' ]) ? $response[ 'success' ] : true;
-$status_code    = isset($response[ 'status_code' ]) ? $response[ 'status_code' ] : 200;
+$count          = $response[ 'count' ] ?? count($response);
+$result         = $response[ 'result' ] ?? $response;
+$success        = $response[ 'success' ] ?? true;
+$status_code    = $response[ 'status_code' ] ?? 200;
 unset($result[ 'count' ], $result[ 'success' ], $result[ 'status_code' ], $result[ 'result' ]);
 echo $response_class->toJson(
 	[ 'success' => $success, 'count' => $count, 'result' => $result ], $status_code
